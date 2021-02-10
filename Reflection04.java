52ba9f7dc78e24f93bf22d092e5bd43ff165b16f
nethminibhagya1524 <77233063+nethminibhagya1524@users.noreply.github.com>
2021-02-10 17:39:42 +0530

    Update Reflection04.java

--- Reflection04.java
+++ Reflection04.java
@@ -3,13 +3,18 @@ package experiments;
 import java.lang.reflect.Field;
 
 public class Reflection04 {
-  public static void main(String[] args) throws Exception {
-    Simple k = new Simple();
-    Field[] fields = k.getClass().getFields();
-    System.out.printf("There are %d fields\n", fields.length);
-    for (Field f : fields) {
-      System.out.printf("field name=%s type=%s value=%d\n", f.getName(),
-          f.getType(), f.getInt(k));
-    }
-  }
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		Simple k = new Simple();
+		
+		Field[] fields = k.getClass().getFields();
+		System.out.printf("There are %d fields\n", fields.length);
+		
+		for(Field f : fields)
+		{
+			System.out.printf("Field Name: %s    Type: %s    Value: %f\n",
+						f.getName(), f.getType(), f.getFloat(k));
+			}
+		}
 }
