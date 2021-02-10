0d58b46cdf8d371ca772c1f212237284f7f165af
nethminibhagya1524 <77233063+nethminibhagya1524@users.noreply.github.com>
2021-02-10 17:45:36 +0530

    Update Reflection10.java

--- Reflection10.java
+++ Reflection10.java
@@ -5,7 +5,7 @@ import java.lang.reflect.*;
 public class Reflection10 {
   public static void main(String[] args) throws Exception {
     Simple k = new Simple();
-    Method m = k.getClass().getDeclaredMethod("setA", float.class);
+    Method m = k.getClass().getDeclaredMethod("setf1", float.class);
     m.setAccessible(true);
     m.invoke(k, 80);
     System.out.println(k);
